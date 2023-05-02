2342683f9f3c0101c51929a098464b903ef296cc
Ash <acst52@gmail.com>
2023-05-02 11:31:07 -0400

    fix eslint-prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: ['plugin:react/recommended', 'airbnb'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
-  rules: {
-    'prettier/prettier': ['error', { singleQuote: true }],
-  },
+  plugins: 'react',
+  rules: {},
 };
--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const MongoDB_URI = process.env.MONGODB_URI || '';
 mongoose.connect(MongoDB_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
