030751aa03d781c390183f8ea502018cc9fdcdef
Ash <acst52@gmail.com>
2023-05-08 12:49:41 -0400

    add to backend code

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,17 @@
 const User = require('./User');
 const Product = require('./Product');
 const Order = require('./Order');
+const Donation = require('./Donation');
+const Message = require('./Message');
+const Resource = require('./Resource');
+const Schedule = require('./Schedule');
 
-module.exports = { User, Product, Order };
+module.exports = {
+  User,
+  Product,
+  Order,
+  Donation,
+  Message,
+  Resource,
+  Schedule,
+};
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,14 @@
 // add order create order - incl spot for price to be passed
 
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Product, Order, Schedule, Message, Donation } = require('../models');
+const {
+  User,
+  Product,
+  Order,
+  Schedule,
+  Message,
+  Donation,
+} = require('../models');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
@@ -56,56 +63,56 @@ const resolvers = {
       }
       throw new AuthenticationError('Not logged in');
     },
-    },
-    order: async (parent, { _id }, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category',
-        });
-
-        return user.orders.id(_id);
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
-      const order = new Order({ products: args.products });
-      const line_items = [];
-
-      const { products } = await order.populate('products');
-
-      for (let i = 0; i < products.length; i++) {
-        const product = await stripe.products.create({
-          name: products[i].name,
-          description: products[i].description,
-          images: [`${url}/images/${products[i].image}`],
-        });
-
-        const price = await stripe.prices.create({
-          product: product.id,
-          unit_amount: products[i].price * 100,
-          currency: 'usd',
-        });
-
-        line_items.push({
-          price: price.id,
-          quantity: 1,
-        });
-      }
-
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items,
-        mode: 'payment',
-        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`,
+  },
+  order: async (parent, { _id }, context) => {
+    if (context.user) {
+      const user = await User.findById(context.user._id).populate({
+        path: 'orders.products',
+        populate: 'category',
       });
 
-      return { session: session.id };
-    },
+      return user.orders.id(_id);
+    }
+
+    throw new AuthenticationError('Not logged in');
   },
+  checkout: async (parent, args, context) => {
+    const url = new URL(context.headers.referer).origin;
+    const order = new Order({ products: args.products });
+    const line_items = [];
+
+    const { products } = await order.populate('products');
+
+    for (let i = 0; i < products.length; i++) {
+      const product = await stripe.products.create({
+        name: products[i].name,
+        description: products[i].description,
+        images: [`${url}/images/${products[i].image}`],
+      });
+
+      const price = await stripe.prices.create({
+        product: product.id,
+        unit_amount: products[i].price * 100,
+        currency: 'usd',
+      });
+
+      line_items.push({
+        price: price.id,
+        quantity: 1,
+      });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items,
+      mode: 'payment',
+      success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${url}/`,
+    });
+
+    return { session: session.id };
+  },
+
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
@@ -116,8 +123,13 @@ const resolvers = {
 
     createSchedule: async (parent, { weekStartDate }, context) => {
       if (context.user) {
-        const schedule = await Schedule.create({ weekStartDate, owner: context.user._id });
-        await User.findByIdAndUpdate(context.user._id, { $push: { schedules: schedule } });
+        const schedule = await Schedule.create({
+          weekStartDate,
+          owner: context.user._id,
+        });
+        await User.findByIdAndUpdate(context.user._id, {
+          $push: { schedules: schedule },
+        });
         return schedule;
       }
       throw new AuthenticationError('Not logged in');
@@ -177,7 +189,7 @@ const resolvers = {
 
       return { token, user };
     },
-  }
-
+  },
+};
 
 module.exports = resolvers;
