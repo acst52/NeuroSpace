e4f831c87805ce7587bca6a67ea23fa9ac28b180
Teri Phillips <phillips.terianne@gmail.com>
2023-05-10 19:38:45 -0400

    calendar

--- client/src/components/calendar.js
+++ client/src/components/calendar.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
-
-
 function Calendar() {
-
   return (
-    <div className='contentBody'>
+    <div className="contentBody">
       <h1 className="title">DASHBOARD - Calendar</h1>
-      <section className = "calendar">
-      </section>
+      <section className="calendar"></section>
     </div>
   );
 }
-
 
 export default Calendar;
