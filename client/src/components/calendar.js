7b32b77d32c4b59c2099cbfa12d5096de096edbb
Teri Phillips <phillips.terianne@gmail.com>
2023-05-11 19:01:39 -0400

    calendar functionality

--- client/src/components/calendar.js
+++ client/src/components/calendar.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 function Calendar() {
+  const [events, setEvents] = useState([
+    { title: 'Event 1', date: '2023-05-12' },
+    { title: 'Event 2', date: '2023-05-11' }
+  ]);
+
+  const handleDateClick = (arg) => {
+    const title = prompt('Enter event title:');
+    if (title) {
+      const newEvent = { title, date: arg.dateStr };
+      setEvents([...events, newEvent]);
+    }
+  };
+
   return (
     <div className="contentBody">
       <h1 className="title">DASHBOARD - Calendar</h1>
-      <section className="calendar"></section>
+      <section className="calendar">
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          dateClick={handleDateClick}
+          initialView="dayGridMonth"
+          weekends={true}
+          events={events}
+        />
+      </section>
     </div>
   );
 }
 
 export default Calendar;
+
