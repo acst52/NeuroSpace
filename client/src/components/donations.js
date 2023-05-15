a7952d6a8fda9d2d74666aa80e0000fdab8071b9
Maximos Sarmpas <maximakos.s@gmail.com>
2023-05-14 21:54:08 -0400

    fnew files added

---
+++ client/src/components/donations.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Donation() {
+
+  return (
+    <div className='footer'>
+      <button><Link to="/donate" className="btnLink">Donate!</Link></button>
+    </div>
+  );
+}
+
+
+export default Donation;
